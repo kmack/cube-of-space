714c1a9de2db70f112062a5a0f2431201d0abf8d
Kevin Mack <kmack01@gmail.com>
2025-08-28 16:13:20 -0700

    Updated ESLint and Prettier to enforce Google TypeScript style

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,35 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import googleConfig from "eslint-config-google";
+import prettierConfig from "eslint-config-prettier";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default [
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
+      parser: tseslint.parser,
       ecmaVersion: 2020,
+      sourceType: "module",
       globals: globals.browser,
     },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier: require("eslint-plugin-prettier"),
+    },
+    extends: [
+      googleConfig,
+      tseslint.configs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+      prettierConfig,
+    ],
+    rules: {
+      "prettier/prettier": "error", // Ensures Prettier formatting is enforced
+    },
   },
-])
+];

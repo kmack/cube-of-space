7f785dd381717cb7c6122ca23daf4ed6152a163c
Kevin Mack <kmack@kaleis.io>
2025-09-13 15:33:29 -0700

    removed debug label

--- src/components/cube-of-space-scene.tsx
+++ src/components/cube-of-space-scene.tsx
@@ -10,7 +10,6 @@ import { WireCube } from './wire-cube';
 import { FaceLabels } from './face-labels';
 import { EdgeLabels } from './edge-labels';
 import { MotherLabels } from './mother-labels';
-import { DebugRichLabel } from './debug-rich-label';
 
 import { HALF } from '../data/constants';
 
@@ -60,7 +59,6 @@ export function CubeOfSpaceScene(): React.JSX.Element {
       {/* Geometry */}
       <FacePlanes opacity={0.8} />
       <WireCube />
-      <DebugRichLabel />
       <FaceLabels />
       <EdgeLabels />
       <MotherLabels />
